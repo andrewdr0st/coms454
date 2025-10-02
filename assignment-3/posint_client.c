b00b93240e483294ab4734c52732b087940e8a20
andrew <anddrost@iastate.edu>
2025-10-01 20:21:07 -0500

    client code

--- assignment-3/posint_client.c
+++ assignment-3/posint_client.c
@@ -42,23 +42,54 @@ pos_int_prog_1(char *host)
 int
 main (int argc, char *argv[])
 {
-	char *host;
-
 	if (argc < 4) {
-		printf ("usage: %s server_host command_name param(s)\n", argv[0]);
+		printf ("usage: %s server_host request_name param(s)\n", argv[0]);
 		exit (1);
 	}
-	host = argv[1];
-	pos_int_prog_1 (host);
 
-	if (strcmp(argv[2], "insert") {
+	//connect client to sertver
+	CLIENT* clnt = clnt_create(argv[1], POS_INT_PROG, POS_INT_VERS, "udp");
 
-	} else if (strcmp(argv[2], "retrieve") {
-
-	} else if (strcmp(argv[2], "delete") {
-
+	//check if request is valid, then perform it
+	if (strcmp(argv[2], "insert") == 0) {
+		if (argc < 5) {
+			printf("insert requires 2 params\n");
+			exit(1);
+		}
+		insert_args args;
+		args.new_element = atoi(argv[3]);
+		if (args.new_element <= 0) {
+			printf("new_element must be positive\n");
+			exit(1);
+		}
+		args.index = atoi(argv[4]);
+		if (args.index < 0) {
+			printf("index must be >= 0\n");
+			exit(1);
+		}
+		insert_1(&args, clnt);
+	} else if (strcmp(argv[2], "retrieve") == 0) {
+		int index = atoi(argv[3]);
+		if (index < 0) {
+			printf("index must be >= 0\n");
+			exit(1);
+		}
+		int* result = retrieve_1(&index, clnt);
+		if (*result == -1) {
+			printf("no item at index %d\n", index);
+		} else {
+			printf("retrieved %d\n", *result);
+		}
+	} else if (strcmp(argv[2], "delete") == 0) {
+		int index = atoi(argv[3]);
+		if (index < 0) {
+			printf("index must be >= 0\n");
+			exit(1);
+		}
+		delete_1(&index, clnt);
 	} else {
-
+		printf("invalid request\naccepted requests are:\ninsert new_element index\nretrieve index\ndelete index\n");
+		exit(1);
 	}
 	exit (0);
 }
